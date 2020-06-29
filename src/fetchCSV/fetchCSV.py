10da4fdf81b14ffe202284706dc080ee55048b32
flamestro <19ahmet99@gmail.com>
2020-06-29 02:44:48 +0200

    switch external products store to minio instance

--- src/fetchCSV/fetchCSV.py
+++ src/fetchCSV/fetchCSV.py
@@ -1,7 +1,6 @@
 import io
 import os
 
-from urllib.request import urlopen
 from minio import Minio
 from minio.error import ResponseError
 
@@ -11,6 +10,7 @@ except ImportError:
     from src.utils.zipsendkin import Span, TraceContext, generate_id
 
 trace_context = None
+_DATA_STORE_ENDPOINT_ = '192.168.178.62'
 
 
 class CsvFile(object):
@@ -27,14 +27,22 @@ class CsvFile(object):
         return csv_lines_list
 
 
-def fetch_csv_file(url):
+def fetch_csv_file(csv_url):
     """
     fetches a csv file from a given url and returns it
-    :param url: a url to an file hosted on a ftp server
+    :param url: a url to an file hosted on a minio server
     :return: returns a csv file as instance of CsvFile
     """
+    splitted_csv_url = csv_url.split("/")
+    filename = splitted_csv_url[-1]
+    bucket = splitted_csv_url[-2]
+    endpoint = splitted_csv_url[-3]
     with Span(span_name='fetch_csv_file', trace_context=trace_context):
-        file = urlopen(url)
+        minio_client = Minio('{}'.format(endpoint),
+                             access_key='AKIAIOSFODNN7EXAMPLE',
+                             secret_key='wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY',
+                             secure=False)
+        file = minio_client.get_object(bucket, filename)
         with io.BytesIO(file.read()) as f:
             csv_file = CsvFile(file=f.read(), size=f.getbuffer().nbytes)
     return csv_file
@@ -49,7 +57,7 @@ def save_file_in_minio(csv_file, shop_key):
     """
     with Span(span_name='save_file_in_minio', trace_context=trace_context):
         filename = "{}:{}".format(generate_id(), shop_key)
-        minio_client = Minio('192.168.178.62:9991',
+        minio_client = Minio('{}:9991'.format(_DATA_STORE_ENDPOINT_),
                              access_key='AKIAIOSFODNN7EXAMPLE',
                              secret_key='wJalrXUtnFEMIK7MDENGbPxRfiCYEXAMPLEKEY',
                              secure=False)
@@ -64,25 +72,27 @@ def save_file_in_minio(csv_file, shop_key):
 
 
 def main(args):
-    # Initialize trace context
-    global trace_context
-    trace_context = TraceContext(trace_id=args.get('__OW_TRACE_ID', os.environ.get('__OW_TRACE_ID', generate_id())),
-                                 service_name='fetchCSV',
-                                 transaction_id=os.environ.get('__OW_TRANSACTION_ID', ''),
-                                 tracer_endpoint='192.168.178.62',
-                                 parent_id=args.get('__PARENT_TRACE_ID', ''),
-                                 action_trace_id=os.environ.get('__OW_TRACE_ID', ''))
-    # Initialize parameters
-    ftp_csv_url = args.get("ftpCsvUrl")
-    shop_key = args.get("shopKey")
+    try:
+        # Initialize trace context
+        global trace_context
+        trace_context = TraceContext(trace_id=args.get('__OW_TRACE_ID', os.environ.get('__OW_TRACE_ID', generate_id())),
+                                     service_name='fetchCSV',
+                                     transaction_id=os.environ.get('__OW_TRANSACTION_ID', ''),
+                                     tracer_endpoint=_DATA_STORE_ENDPOINT_,
+                                     parent_id=args.get('__PARENT_TRACE_ID', ''),
+                                     action_trace_id=os.environ.get('__OW_TRACE_ID', ''))
+        # Initialize parameters
+        csv_url = args.get("csvUrl", "http://{}:9990/productdata/test.csv".format(_DATA_STORE_ENDPOINT_))
+        shop_key = args.get("shopKey", "771d87188d568ddd")
+        # get csv file (CsvFile instance)
+        file = fetch_csv_file(csv_url)
+        # save csv file in minio and return filename
+        minio_save_result = save_file_in_minio(file, shop_key)
+        return {'message': str(minio_save_result)}
+    except Exception as e:
+        print(e)
+    return {'error': "could not fetch data properly"}
 
-    # get csv file (CsvFile instance)
-    file = fetch_csv_file(ftp_csv_url)
-    # save csv file in minio and return filename
-    print(file.csv_lines())
-    minio_save_result = save_file_in_minio(file, shop_key)
-    return {'message': minio_save_result}
 
-
-main({"ftpCsvUrl": "ftp://localhost/test/test.csv",
-      "shopKey": "771d87188d568ddd"})
+if __name__ == "__main__":
+    main({"csvUrl": "http://{}:9990/productdata/test.csv".format(_DATA_STORE_ENDPOINT_), "shopKey": "771d87188d568ddd"})
